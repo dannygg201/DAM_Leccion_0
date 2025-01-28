2043c5486eb54d87cfdfacb2df291ac899874fe0
Danny González Silva <Dannygg201@outlook.es>
2025-01-28 17:48:52 -0600

    Version_01: Se creo el proyecto MAUI vacío.

---
+++ DAM_Leccion_DGS/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DAM_Leccion_DGS
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DAM_Leccion_DGS/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DAM_Leccion_DGS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
